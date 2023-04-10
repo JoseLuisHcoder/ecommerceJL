4ac25fc7ae595a811243a9e4cf0c09b3c27ea3cd
Jose Luis <zonajl@gmail.com>
2023-04-10 08:20:11 -0400

    init project

---
+++ src/store/slices/isLoading.slice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const isLoadingSlice = createSlice({
+	name: 'isLoading',
+	initialState: false,
+	reducers: {
+		setIsLoadingGlobal: (state, action) => action.payload,
+	},
+})
+
+export const { setIsLoadingGlobal } = isLoadingSlice.actions
+export default isLoadingSlice.reducer
